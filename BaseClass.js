7462d6aef51dbfcacc505060b3f37d5b73c7b2c2
soham2315 <74770280+soham2315@users.noreply.github.com>
2020-12-08 18:00:02 +0530

    Add files via upload

---
+++ BaseClass.js
@@ -0,0 +1,26 @@
+class BaseClass {
+    constructor(x, y,width,height,angle) {
+      var options = {
+        'density':1.5,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 50;
+      this.height = 50;
+      this.image = loadImage ("sprites/base.png")
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER)
+      image(this.image,0, 0, this.width,this.height);
+      pop();
+    };
+  };
+  
